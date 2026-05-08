d0bb063822a1f4dfd27cc5b7bc02367448bdf707
Jose Victor <jovicx2007@protonmail.com>
2026-05-08 11:50:43 +0100

    Levels
    The main changes are now leves are now implemented and also, get current level so if its gameover you retry the previous level

--- Board.java
+++ Board.java
@@ -10,22 +10,204 @@ public class Board {
 
     public static final int ROWS = 4;
     public static final int COLS = 5; // sets the grid 
-
+    public static final int MAX_LEVEL = 16;
+    
     private int[][] grid;
+    private int currentLevel;
 
     public Board() {
         grid = new int[ROWS][COLS];
         Level1();
     }
+    public int getCurrentLevel() {
+        return currentLevel;
+    }
+        public void loadLevel(int level) {
+        if (level < 1 || level > MAX_LEVEL) {
+            return;
+        }
 
-    public void Level1() { // first level
-        grid = new int[ROWS][COLS];
-        grid[3][0] = SMALL_SNOWBALL;
-        grid[3][2] = BIG_SNOWBALL;
-        grid[3][3] = TREE;
-        grid[2][2] = SNOWMAN_HEAD;
+        switch (level) {
+            case 1: Level1(); break;
+            case 2: Level2(); break;
+            case 3: Level3(); break;
+            case 4: Level4(); break;
+            case 5: Level5(); break;
+            case 6: Level6(); break;
+            case 7: Level7(); break;
+            case 8: Level8(); break;
+            case 9: Level9(); break;
+            case 10: Level10(); break;
+            case 11: Level11(); break;
+            case 12: Level12(); break;
+            case 13: Level13(); break;
+            case 14: Level14(); break;
+            case 15: Level15(); break;
+            case 16: Level16(); break;
+        }
     }
 
+    public void Level1() {
+        currentLevel = 1;
+        clearBoard();
+        grid[0][0] = BIG_SNOWBALL;
+        grid[0][4] = SNOWMAN_HEAD;
+        grid[0][3] = SMALL_SNOWBALL;
+        grid[3][4] = TREE;
+    }
+
+    public void Level2() {
+        currentLevel = 2;
+        clearBoard();
+        grid[1][0] = SMALL_SNOWBALL;
+        grid[1][3] = TREE;
+        grid[2][1] = SNOWMAN_HEAD;
+        grid[2][4] = BIG_SNOWBALL;
+    }
+
+    public void Level3() {
+        currentLevel = 3;
+        clearBoard();
+        grid[0][3] = TREE;
+        grid[1][0] = SNOWMAN_HEAD;
+        grid[2][1] = SMALL_SNOWBALL;
+        grid[3][1] = BIG_SNOWBALL;
+        grid[3][4] = TREE;
+    }
+
+    public void Level4() {
+        currentLevel = 4;
+        clearBoard();
+        grid[0][0] = BIG_SNOWBALL;
+        grid[0][4] = SNOWMAN_HEAD;
+        grid[3][0] = SMALL_SNOWBALL;
+        grid[3][4] = TREE;
+    }
+
+    public void Level5() {
+        currentLevel = 5;
+        clearBoard();
+        grid[0][0] = BIG_SNOWBALL;
+        grid[0][4] = SMALL_SNOWBALL;
+        grid[2][2] = SNOWMAN_HEAD;
+        grid[3][0] = TREE;
+        grid[3][2] = TREE;
+        grid[3][4] = TREE;
+    }
+
+    public void Level6() {
+        currentLevel = 6;
+        clearBoard();
+        grid[0][2] = TREE;
+        grid[2][1] = BIG_SNOWBALL;
+        grid[2][3] = SMALL_SNOWBALL;
+        grid[2][4] = SNOWMAN_HEAD;
+        grid[3][2] = TREE;
+    }
+
+    public void Level7() {
+        currentLevel = 7;
+        clearBoard();
+        grid[0][2] = BIG_SNOWBALL;
+        grid[0][4] = SMALL_SNOWBALL;
+        grid[1][0] = SNOWMAN_HEAD;
+        grid[2][2] = TREE;
+        grid[2][3] = TREE;
+    }
+
+    public void Level8() {
+        currentLevel = 8;
+        clearBoard();
+        grid[0][2] = SNOWMAN_HEAD;
+        grid[1][0] = TREE;
+        grid[3][0] = BIG_SNOWBALL;
+        grid[3][1] = SMALL_SNOWBALL;
+        grid[3][3] = TREE;
+    }
+
+    public void Level9() {
+        currentLevel = 9;
+        clearBoard();
+        grid[0][0] = TREE;
+        grid[0][3] = BIG_SNOWBALL;
+        grid[2][1] = SNOWMAN_HEAD;
+        grid[3][2] = SMALL_SNOWBALL;
+        grid[3][4] = TREE;
+    }
+
+    public void Level10() {
+        currentLevel = 10;
+        clearBoard();
+        grid[0][0] = BIG_SNOWBALL;
+        grid[0][3] = SMALL_SNOWBALL;
+        grid[1][1] = SNOWMAN_HEAD;
+        grid[2][3] = TREE;
+        grid[3][2] = TREE;
+        grid[3][4] = TREE;
+    }
+
+    public void Level11() {
+        currentLevel = 11;
+        clearBoard();
+        grid[0][4] = BIG_SNOWBALL;
+        grid[1][0] = TREE;
+        grid[1][4] = SMALL_SNOWBALL;
+        grid[3][1] = SNOWMAN_HEAD;
+        grid[3][3] = TREE;
+        grid[3][4] = TREE;
+    }
+
+    public void Level12() {
+        currentLevel = 12;
+        clearBoard();
+        grid[1][1] = SMALL_SNOWBALL;
+        grid[1][3] = SNOWMAN_HEAD;
+        grid[3][0] = TREE;
+        grid[3][3] = BIG_SNOWBALL;
+        grid[3][4] = TREE;
+    }
+
+    public void Level13() {
+        currentLevel = 13;
+        clearBoard();
+        grid[0][0] = BIG_SNOWBALL;
+        grid[2][0] = SNOWMAN_HEAD;
+        grid[2][3] = TREE;
+        grid[3][2] = SMALL_SNOWBALL;
+        grid[3][4] = TREE;
+    }
+
+    public void Level14() {
+        currentLevel = 14;
+        clearBoard();
+        grid[0][0] = BIG_SNOWBALL;
+        grid[0][2] = SNOWMAN_HEAD;
+        grid[1][4] = SMALL_SNOWBALL;
+        grid[2][1] = TREE;
+        grid[3][3] = TREE;
+    }
+
+    public void Level15() {
+        currentLevel = 15;
+        clearBoard();
+        grid[0][2] = TREE;
+        grid[1][4] = BIG_SNOWBALL;
+        grid[2][0] = SNOWMAN_HEAD;
+        grid[3][0] = SMALL_SNOWBALL;
+        grid[3][3] = TREE;
+        grid[3][4] = TREE;
+    }
+
+    public void Level16() {
+        currentLevel = 16;
+        clearBoard();
+        grid[0][3] = TREE;
+        grid[0][4] = BIG_SNOWBALL;
+        grid[1][4] = SMALL_SNOWBALL;
+        grid[2][0] = SNOWMAN_HEAD;
+        grid[3][3] = TREE;
+        grid[3][4] = TREE;
+    }
     public boolean isInsideBoard(int row, int col) {                
         return row >= 0 && row < ROWS && col >= 0 && col < COLS;
     }
@@ -82,7 +264,7 @@ public class Board {
                 newBoard.grid[row][col] = this.grid[row][col];
             }
         }
-
+            newBoard.currentLevel = this.currentLevel; //the solver will the know which level belogns this way
+        
         return newBoard;
     }
-}
